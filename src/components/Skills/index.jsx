6523ccde5013dd76693a1f58d8dc7ac810550ed7
NAbdullokh <country.abu@gmail.com>
2022-08-27 02:02:34 +0500

    mock

--- src/components/Skills/index.jsx
+++ src/components/Skills/index.jsx
@@ -1,26 +1,27 @@
-import axios from "axios";
-import React, { useEffect, useState } from "react";
+// import axios from "axios";
+import React, { useEffect } from "react";
 import { Card, CardWrapper, Container, Wrapper } from "./style";
 import { FaLaptopMedical } from "react-icons/fa";
 import Aos from "aos";
+import { Languages } from "../../mock/skills";
 
 const Skills = () => {
   useEffect(() => {
     Aos.init({ duration: 1500 });
   }, []);
-  const [data, setData] = useState([]);
-  const [isLoading, setLoading] = useState(true);
+  // const [data, setData] = useState([]);
+  // const [isLoading, setLoading] = useState(true);
 
-  useEffect(() => {
-    axios
-      .get("http://udontnow.pythonanywhere.com/api/v4/image/")
-      .then((res) => setData(res.data));
-    setLoading(false);
-  }, []);
+  // useEffect(() => {
+  //   axios
+  //     .get("http://udontnow.pythonanywhere.com/api/v4/image/")
+  //     .then((res) => setData(res.data));
+  //   setLoading(false);
+  // }, []);
 
-  if (isLoading) {
-    return <h1 style={{ color: "white" }}>Loading...</h1>;
-  }
+  // if (isLoading) {
+  //   return <h1 style={{ color: "white" }}>Loading...</h1>;
+  // }
 
   return (
     <Wrapper id="skills">
@@ -30,13 +31,10 @@ const Skills = () => {
           Skills & <span>Abilities</span>
         </Container.Title>
         <CardWrapper>
-          {data.map((value) => {
+          {Languages.map((value) => {
             return (
               <Card data-aos="zoom-in" key={value.id}>
-                <Card.Img
-                  src={`http://udontnow.pythonanywhere.com/${value.icon}`}
-                  alt=""
-                />
+                <Card.Img src={value.icon} alt="" />
                 <Card.Title>{value.title}</Card.Title>
               </Card>
             );
---
+++ src/mock/skills.js
@@ -0,0 +1,93 @@
+import react from "../assets/img/skills/react.png";
+import css from "../assets/img/skills/css.png";
+import antd from "../assets/img/skills/djsa.png";
+import figma from "../assets/img/skills/figma.png";
+import github from "../assets/img/skills/github.png";
+import heroku from "../assets/img/skills/heroku.png";
+import html from "../assets/img/skills/html.png";
+import js from "../assets/img/skills/js.png";
+import mui from "../assets/img/skills/mui.png";
+import netlify from "../assets/img/skills/netlify.png";
+import styled from "../assets/img/skills/styled.png";
+import tailwind from "../assets/img/skills/tailwind.png";
+import git from "../assets/img/skills/git.png";
+import telegram from "../assets/img/skills/telegram.jpg";
+import rest from "../assets/img/skills/rest.png";
+
+export const Languages = [
+  {
+    id: 5,
+    title: "React JS",
+    icon: react,
+  },
+  {
+    id: 7,
+    title: "Javascript",
+    icon: js,
+  },
+  {
+    id: 8,
+    title: "Html",
+    icon: html,
+  },
+  {
+    id: 9,
+    title: "Css",
+    icon: css,
+  },
+  {
+    id: 10,
+    title: "Tailwind Css",
+    icon: tailwind,
+  },
+  {
+    id: 11,
+    title: "Styled-Components",
+    icon: styled,
+  },
+  {
+    id: 12,
+    title: "Netlify",
+    icon: netlify,
+  },
+  {
+    id: 13,
+    title: "GitHub",
+    icon: github,
+  },
+  {
+    id: 14,
+    title: "Heroku",
+    icon: heroku,
+  },
+  {
+    id: 16,
+    title: "Antd Design",
+    icon: antd,
+  },
+  {
+    id: 17,
+    title: "MaterialUI",
+    icon: mui,
+  },
+  {
+    id: 18,
+    title: "Figma",
+    icon: figma,
+  },
+  {
+    id: 19,
+    title: "Telegram Bot",
+    icon: telegram,
+  },
+  {
+    id: 20,
+    title: "Rest Full Api",
+    icon: rest,
+  },
+  {
+    id: 21,
+    title: "Git VCS",
+    icon: git,
+  },
+];
